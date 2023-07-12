fee54f5c0e63a22db2909aee13be0b8d4a6d56f3
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-07-12 11:50:10 +0200

    feat: register, login and logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Laravel\Sanctum\PersonalAccessToken;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'name' => 'required|string',
+            'surname' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', Password::min(8)->mixedCase()->numbers()],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        $validData = $validator->validated();
+
+        $newUser = User::create([
+            'name' => $validData['name'],
+            'surname' => $validData['surname'],
+            'email' => $validData['email'],
+            'password' => bcrypt($validData['password']),
+            'role_id' => 2
+        ]);
+
+        $token = $newUser->createToken('apiToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'user' => $newUser,
+                'token' => $token
+            ]
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $req)
+    {
+        try {
+            $validator = Validator::make($req->all(), [
+                'email' => 'required|email|',
+                'password' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+            $validData = $validator->validated();
+
+            $user = User::where('email', $validData['email'])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Email or password incorrect'
+                ], Response::HTTP_FORBIDDEN);
+            }
+
+            $token = $user->createToken('apiToken')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'token' => $token,
+                    'user' => $user,
+                ]
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::error('Error getting tasks' . $th->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function logout(Request $req)
+    {
+        try {
+            $headerToken = $req->bearerToken();
+            $token = PersonalAccessToken::findToken($headerToken);
+            $token->delete();
+
+            return response()->json([
+                'success' => true,
+                'data' => []
+            ], Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            Log::error('Error getting tasks' . $th->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function profile()
+    {
+        try {
+            $user = auth()->user();
+
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'user' => $user,
+                ]
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::error('Error getting tasks' . $th->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,41 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
-use App\Models\User;
-use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::get('/user/{userId}', function ($userId) {
-    // $user = DB::table('users')->where('id', $userId)->get();
-    // $user = User::where('id', $userId)->get();
-    $user = User::find($userId);
-
-    return [
-        'success' => true,
-        'data' => [
-            'user' => $user
-        ]
-    ];
-}); {
-}
 Route::get('task', [TaskController::class, 'getAllTasks']);
-
 Route::get('task/user/{userId}', [TaskController::class, 'getTasksByUser']);
-
 Route::post('task', [TaskController::class, 'createTask']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('user/profile', [AuthController::class, 'profile']);
